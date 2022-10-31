1457461630c4c41f1637f836fe19c92f3cc4106f
filipboucek <67879023+filipboucek@users.noreply.github.com>
2022-10-31 21:33:25 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+
+function onclicktest()
+{
+    alert("test ok");
+}
+var updateClock = window.setInterval(function(){
+    var countDownDate = new Date("Dec 1, 2022 0:0:0").getTime();
+    var currentDate = new Date().getTime();
+    var timeleft = countDownDate - currentDate; 
+    var dny = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hodiny = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minuty = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var sekundy = Math.floor((timeleft % (1000 * 60)) / 1000);
+    var element = document.getElementById("countdown-clock");
+    // var rawclock = document.getElementById("raw-clock");
+    rawclock.innerHTML = timeleft;
+    element.innerHTML = dny + ":" + hodiny + ":" + minuty + ":" + sekundy;
+  }, 1000);
+function updateText() 
+{
+    
+
+}
+function updateClock() {
+
+}
